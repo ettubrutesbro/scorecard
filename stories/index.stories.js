79a6f40f992b94c69c2391a0e08df137b9443e45
Jack L <jack.leng@gmail.com>
2018-06-13 16:42:28 -0700

    imported components from old childrennow repo; got mock_indicators to reflect in interactivemap

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -8,11 +8,15 @@ import {withKnobs, select} from '@storybook/addon-knobs'
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
+import CaliforniaCountyMap from '../src/components/CountyMap'
+import {counties} from '../src/assets/counties'
+import {indicators} from '../src/assets/mock_indicators'
+import App from '../src/App'
 
 import { Button, Welcome } from '@storybook/react/demo';
 
-	addDecorator(withKnobs)
-addDecorator(withViewport('iphone6'))
+  addDecorator(withKnobs)
+// addDecorator(withViewport('iphone6'))
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -27,10 +31,35 @@ storiesOf('Button', module)
   ));
 
 storiesOf('knob test', module).add('yo', ()=>{
-	const knob = select('test', ['hello', 'world'])
-	return(
-		<div className = {styles.testModule}>
-			{knob}
-		</div>
-	)
+  const knob = select('test', ['hello', 'world'])
+  return(
+    <div className = {styles.testModule}>
+      {knob}
+    </div>
+  )
+})
+
+storiesOf('scorecard', module).add('CountyMap', ()=>{
+  return(
+    <div>
+     <CaliforniaCountyMap 
+      mode = "heat"
+      data = {indicators[0]['year_2017']}
+     />
+
+    </div>
+  )
+})
+.add('mock indicators',()=>{
+  return(
+    <div>
+      {indicators.map((ind)=>{
+        return <span> {ind.id}</span>
+      })}
+    </div>
+  )
+
+})
+.add('App', ()=>{
+    return <App />
 })
