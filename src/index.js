1850361c60c73745cd81361c4d3bbcc9b3f728ec
Jack L <jack.leng@gmail.com>
2018-08-10 13:59:01 -0700

    prep for netlify

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Scorecard from './Scorecard';
+import NeoScorecard from './NeoScorecard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Scorecard />, document.getElementById('root'));
+ReactDOM.render(<NeoScorecard />, document.getElementById('root'));
 registerServiceWorker();
