926b3c86e5af9da4574da6cdb10c9ef86395c389
Jack L <jack.leng@gmail.com>
2018-08-10 23:05:23 -0700

    oops: rm'd tippy mention that was derailing deployment

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,5 @@
 import { configure } from '@storybook/react';
-import 'react-tippy/dist/tippy.css';
-// import '../src/global.css'
+import '../src/global.css'
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
