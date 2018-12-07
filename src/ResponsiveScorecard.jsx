be8f266515afe5c133c5f0ddbeb6a84ce715d34a
Jack L <jack.leng@gmail.com>
2018-12-07 00:22:41 -0800

    greymask now has pointer-events enabled, stopping selection of source/pdf/race table...why was it that way in the first place?

--- src/ResponsiveScorecard.jsx
+++ src/ResponsiveScorecard.jsx
@@ -152,7 +152,7 @@ const BottomRow = styled(Row)`
 
 const GreyMask = styled.div`
     position: absolute;
-    pointer-events: none;
+    /*pointer-events: none;*/
     left: 0;
     top: 0;
     width: 100%;
@@ -255,7 +255,6 @@ const SourcesButton = styled(Button)`
             }
         }
     `}
-    
 `
 
 @observer
