ab1dd421b623f4c2c80e0d9433ac931bd6430707
Jack L <jack.leng@gmail.com>
2018-12-16 14:18:10 -0800

    lol

---
+++ src/MobileScorecard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {observable, action} from 'mobx'
+import {observer} from 'mobx-react'
+import styled from 'styled-components'
+
+export default class MobileScorecard extends React.Component{
+    
+    render(){
+        return(
+            <div>
+                Hullo!
+            </div>
+        )
+    }
+}
