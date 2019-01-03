a92dfb7f273d2a7124f27d3d6419fe4b04977f1a
Jack L <jack.leng@gmail.com>
2019-01-03 14:11:51 -0800

    FAH with focus events for searching, but gonna go in a diff direction

--- src/components/FixedActionsHelper.jsx
+++ src/components/FixedActionsHelper.jsx
@@ -1,31 +1,67 @@
 import React from 'react'
+import {observable, action} from 'mobx'
+import {observer} from 'mobx-react'
 import styled from 'styled-components'
+
 import ExpandBox from './ExpandBox'
 import Icon from './generic/Icon'
 
-const FixedActionsHelper = (props) => {
-    return(
-        <Fah
-            currentMode = {props.mode}
-            modes = {{
-                collapsed: {width: 5, height: 5},
-                expanded: {width: 48, height: 100},
-                xOnly: {width: 48, height: 48},
-                searching: {width: 275, height: 48},
-            }}
-            borderColor = 'var(--fainttext)'
-        >
-            <Icons
-                mode = {props.mode}
+@observer
+export default class FixedActionsHelper extends React.Component {
+    @observable searchMaskOn = false
+    constructor(){
+        super()
+        this.searchInput = React.createRef()
+    }
+    componentDidUpdate(prevProps){
+        if(prevProps.mode !== this.props.mode){
+            if(this.props.mode === 'searching'){
+                this.searchInput.current.focus()
+            }
+            else{
+                //clear search string..?
+                this.searchInput.current.blur()
+            }
+        }
+    }
+    focusSearch = (tf) => {
+        if(tf) this.searchMaskOn = true
+        else this.searchMaskOn = false
+    }
+    render(){
+        const props = this.props
+        return(
+            <React.Fragment>
+            <Fah
+                currentMode = {props.mode}
+                modes = {{
+                    collapsed: {width: 5, height: 5},
+                    expanded: {width: 48, height: 100},
+                    xOnly: {width: 48, height: 48},
+                    searching: {width: 275, height: 48},
+                }}
+                borderColor = 'var(--fainttext)'
+                backgroundColor = 'white'
             >
-                <Btn mode = {props.mode}><XIcon img = "x" color = 'normtext'/></Btn>
-                <Btn mode = {props.mode}><SearchIcon img = "searchzoom" color = {props.mode==='searching'? 'fainttext' : 'normtext'}/></Btn>
-                <SearchInput 
-                    placeholder = 'Type to search...'
-                />
-            </Icons>
-        </Fah>
-    )
+                <Icons
+                    mode = {props.mode}
+                >
+                    <Btn mode = {props.mode}><XIcon img = "x" color = 'normtext'/></Btn>
+                    <Btn mode = {props.mode}><SearchIcon img = "searchzoom" color = {props.mode==='searching'? 'fainttext' : 'normtext'}/></Btn>
+                    <SearchInput 
+                        ref = {this.searchInput}
+                        placeholder = 'Type to search...'
+                        onFocus = {()=> this.focusSearch(true)}
+                        onBlur = {()=> this.focusSearch(false)}
+                    />
+
+                </Icons>
+                
+            </Fah>
+            <MaskForSearch on = {this.searchMaskOn} />
+            </React.Fragment>
+        )
+    }
 }
 
 const Fah = styled(ExpandBox)`
@@ -33,6 +69,7 @@ const Fah = styled(ExpandBox)`
     right: 20px; top: 20px;
     transform: translateX(${props => -props.modes[props.currentMode].width}px);
     transition: transform .35s cubic-bezier(0.215, 0.61, 0.355, 1);
+    z-index: 20;
 `
 const Icons = styled.div`
     position: absolute;
@@ -68,4 +105,12 @@ const SearchInput = styled.input`
     font-size: 16px;
 `
 
-export default FixedActionsHelper
+const MaskForSearch = styled.div`
+    position: absolute;
+    left: 0; top: 0;
+    width: 100%; height: 100%;
+    background: red;
+    display: ${props => props.on? 'block' : 'none'};
+    z-index: 19;
+    opacity: 0.25;
+`
