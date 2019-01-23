51b84fcc92acbe31ac09cb46513af0b2cada01c1
Jack Leng <jack.leng@gmail.com>
2019-01-23 03:43:20 -0800

    copy string changed to match mobile

--- src/components/InitCopy.js
+++ src/components/InitCopy.js
@@ -2,7 +2,7 @@ const copy = {
 	firstpara: 'Welcome! This tool aims to provide a comprehensive look at how children are doing in California’s 58 counties, with data viewable by year and race/ethnicity.',
 	secondpara: 'Some data is unreported, incomplete or unavailable due to small sample size and/or a high margin of error.',
 	title: '2018-19 California County Scorecard of Children’s Well-Being',
-	compatibility: 'This tool supports Chrome (65 and newer), Safari (10+), and Firefox (54+).'
+	compatibility: 'This tool supports recent versions of Chrome, Safari, and Firefox on Android, iOS, Mac OS, and Windows.'
 }
 
 export default copy
